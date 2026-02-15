6eef94a4c091a888873f59f173425c350797232b
Runnies23 <nattapongryan230707@gmail.com>
2026-02-15 21:08:06 +0700

    reset

--- download_weight.sh
+++
@@ -1,3 +0,0 @@
-wget  -P "./ASC26-Embodied-World-Model-Optimization/ckpts/" https://huggingface.co/unitreerobotics/UnifoLM-WMA-0-Dual/resolve/main/unifolm_wma_dual.ckpt 
-
-wget  -P "./unifolm-world-model-action/weight_clip" https://huggingface.co/laion/CLIP-ViT-H-14-laion2B-s32B-b79K/resolve/main/open_clip_pytorch_model.bin
